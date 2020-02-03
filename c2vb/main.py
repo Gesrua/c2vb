fee3814896be3a9b7ea7a18f067181723d507dbc
Chaigidel <chaigidel@outlook.com>
2020-02-03 20:21:41 +0800

    warning

--- c2vb/main.py
+++ c2vb/main.py
@@ -8,7 +8,16 @@ def run(file=None, src=None):
     if file != None and src != None:
         raise Exception('Can\'t give src and file at same time')
     f = open(file, 'r')
-    src = f.read()
+    lines = f.readlines()
+    src = ''
+    for i in lines:
+        if i[0] == '#':
+            if i.find('include') == 1:
+                raise Exception('不支持 include')
+            else:
+                print('宏将被忽略')
+        else:
+            src = src + i
     lex = Lexer(src)
     parser = Parser(lex.tokens)
     proc(parser.ast)
