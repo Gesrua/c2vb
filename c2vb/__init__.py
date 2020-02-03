81bf8ba76c266decca10fa61a7d6b01d67705725
Chaigidel <chaigidel@outlook.com>
2020-02-03 22:00:11 +0800

    console

--- c2vb/__init__.py
+++ c2vb/__init__.py
@@ -1,4 +1,5 @@
 from .lexer import Lexer
 from .parser import Parser
 from .main import run
+from .main import console
 from .idt import proc
--- c2vb/main.py
+++ c2vb/main.py
@@ -1,6 +1,7 @@
 from .lexer import Lexer
 from .parser import Parser
 from .idt import proc
+import argparse
 
 def run(file=None, src=None):
     if file == None and src == None:
@@ -23,4 +24,9 @@ def run(file=None, src=None):
     proc(parser.ast)
     return parser.ast.vb()
 
+def console():
+    parser = argparse.ArgumentParser(description='A program convert simple C / C++ code to Visual Basic 6 code')
+    parser.add_argument('file', help='a c/c++ file need to convert')
+    args = parser.parse_args()
+    print(run(file=args.file))
 
